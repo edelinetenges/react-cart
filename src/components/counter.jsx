7a398e1d3899bebca3811d5d51c772706bdcc5c3
edelinetenges <edelinetenges@gmail.com>
2021-05-25 16:48:56 +0800

    Increment counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,11 +7,15 @@ export default class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1});
+    };
+
     render() {
         return (
             <div>
                 <button className={this.getButtonClasses()}>{this.formatCount()}</button>
-                <button className="btn btn-info m-2">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-info m-2">Increment</button>
                 <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul>
