ff2874a847b7b3489bca8580e7cbfcf6c146e190
edelinetenges <edelinetenges@gmail.com>
2021-05-25 15:40:49 +0800

    Display and format count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {
+export default class Counter extends Component {
+    state = {
+        count: 0
+    };
+
     render() {
-        return <h1>Hi there</h1>;
+        return (
+            <div>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>;
+            </div>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
-
-export default Counter;
